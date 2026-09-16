4cfb78e96c01ec9179f283936853590e3203e398
leozaeli <leosj.cc@gmail.com>
2026-09-15 21:02:07 -0300

    Invalida cache do estilo da logo

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="/styles.css?v=20260915-logo-scale" />
       </head>
       <body>
         {children}
