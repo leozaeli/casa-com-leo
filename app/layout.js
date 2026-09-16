1cbb19478da01b4fd26936211e0299bc121958ef
leozaeli <leosj.cc@gmail.com>
2026-09-15 21:39:00 -0300

    Ajusta navegação para lançamentos

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="stylesheet" href="/styles.css?v=20260915-logo-fit" />
+        <link rel="stylesheet" href="/styles.css?v=20260915-launch-nav" />
       </head>
       <body>
         {children}
